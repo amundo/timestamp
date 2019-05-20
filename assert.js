8c3dea2efcb3c15fdf9386d38ac12a248d5b1b53
Patrick Hall <pjh@umail.ucsb.edu>
2019-05-19 21:10:44 -0400

    initial commit

---
+++ assert.js
@@ -0,0 +1,2 @@
+export let assert = ( message, test ) => console.log(test ? `[PASS] ${message}` : `[FAIL] ${message}` )
+
--- timestamps.js
+++ timestamps.js
@@ -1,35 +1,11 @@
-/*
+let secondsToTimestamp = seconds => new Date(seconds * 1000)
+  .toISOString()
+  .slice(11, -1)
 
-The timestamps must be in one of two formats:
-
-    mm:ss.ttt
-    hh:mm:ss.ttt
-
-Where the components are defined as follows:
-
-    hh is hours.
-        Must be at least two digits.
-        Hours can be greater than two digits (e.g., 9999:00:00.000).
-    mm is minutes.
-        Must be between 00 and 59 inclusive.
-    ss is seconds.
-        Must be between 00 and 59 inclusive.
-    ttt is miliseconds.
-        Must be between 000 and 999 inclusive.
-
-*/
-
-let secondsToTimestamp = seconds => {
-  let datetime =  new Date(seconds * 1000).toISOString()
-  // datetime looks like: 1970-01-01T00:00:08.230Z
-  let timestamp = datetime.slice(11, -1)
-  return timestamp
-}
-
-let timestampToSeconds = timestamp => {
-  return timestamp
+let timestampToSeconds = timestamp => timestamp
     .split(':')
-    .reduce((acc,time) => (60 * acc) + +time, 0)
-}
+    .reduce((total, time) => 
+       (60 * total) + parseFloat(time), 
+    0)
 
 export {secondsToTimestamp, timestampToSeconds}
